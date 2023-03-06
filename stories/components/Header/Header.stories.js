5543cc8fa50daade1222642b965a43eec93cc23e
Gustavo <gustavo7dias@gmail.com>
2023-03-06 13:56:18 -0300

    Initial commit

---
+++ stories/components/Header/Header.stories.js
@@ -0,0 +1,8 @@
+import HeaderComponent from "./Header.html";
+import "./Header.css";
+
+export default {
+  title: "Components/Header",
+};
+
+export const header = () => HeaderComponent;
---
+++ stories/components/Menu/Menu.stories.js
@@ -0,0 +1,8 @@
+import MenuComponent from "./Menu.html";
+import "./Menu.css";
+
+export default {
+  title: "Components/Menu",
+};
+
+export const Menu = () => MenuComponent;
