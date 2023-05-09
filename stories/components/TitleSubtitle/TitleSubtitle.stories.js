1dc0058b9513f62d2721e18f8dbbc2af07cc6b3c
Gustavo <gustavo7dias@gmail.com>
2023-05-09 16:32:23 -0300

    Create title-subtitle story

---
+++ stories/components/TitleSubtitle/TitleSubtitle.stories.js
@@ -0,0 +1,9 @@
+import Primary from "./Primary.html";
+import Secondary from "./Secondary.html";
+
+export default {
+  title: "Components/Title and Subtitle",
+};
+
+export const primary = () => Primary;
+export const secondary = () => Secondary;
