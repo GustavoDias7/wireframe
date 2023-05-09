33a352c9c63d9442367172b8e9ccf3e8076b2e21
Gustavo <gustavo7dias@gmail.com>
2023-05-09 17:41:47 -0300

    Create price story

---
+++ stories/components/Price/Price.stories.js
@@ -0,0 +1,11 @@
+import Primary from "./Primary.html";
+import Secondary from "./Secondary.html";
+import Tertiary from "./Tertiary.html";
+
+export default {
+  title: "Components/Price",
+};
+
+export const primary = () => Primary;
+export const secondary = () => Secondary;
+export const _tertiary = () => Tertiary;
