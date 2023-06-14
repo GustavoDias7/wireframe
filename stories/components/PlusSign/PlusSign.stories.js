11719c68e9a471772ab02520f68da96fe83b9d8a
Gustavo <gustavo7dias@gmail.com>
2023-06-14 16:29:36 -0300

    Update plus sign generator

--- stories/components/PlusSign/PlusSign.stories.js
+++ stories/components/PlusSign/PlusSign.stories.js
@@ -6,43 +6,39 @@ export const PlusSign = {
   render: (args) => {
     const div = document.createElement("div");
     const span = document.createElement("span");
-    const pre = document.createElement("pre");
     const style = document.createElement("style");
     const templateStyle = `
-.plus {
-  position: relative;
-  display: block;
-  width: ${args.width}px;
-  height: ${args.width}px;
-}
-.plus::before {
-  content: "";
-  position: absolute;
-  display: block;
-  background-color: ${args.backgroundColor};
-  width: ${args.width}px;
-  height: ${args.height}px;
-  margin: auto;
-  top: 0px;
-  bottom: 0px;
-}
-.plus::after {
-  content: "";
-  position: absolute;
-  display: block;
-  background-color: ${args.backgroundColor};
-  width: ${args.height}px;
-  height: ${args.width}px;
-  margin: auto;
-  left: 0px;
-  right: 0px;
-}`
+  .plus {
+    position: relative;
+    display: block;
+    width: ${args.width}px;
+    height: ${args.width}px;
+  }
+  .plus::before,
+  .plus::after {
+    content: "";
+    position: absolute;
+    margin: auto;
+    background-color: ${args.backgroundColor};
+  }
+  .plus::before {
+    width: ${args.width}px;
+    height: ${args.height}px;
+    top: 0px;
+    bottom: 0px;
+  }
+  .plus::after {
+    width: ${args.height}px;
+    height: ${args.width}px;
+    left: 0px;
+    right: 0px;
+  }\n`
     style.innerHTML = templateStyle
     span.classList.add("plus")
     div.append(style)
+    div.append("\n")
     div.append(span)
-    div.append(pre);
-    return div;
+    return div.innerHTML;
   },
   argTypes: { 
     width: { control: { type: 'number', min:1, step: 1 }},
