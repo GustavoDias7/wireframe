6824bc889971a11206515a35f095c72a7cdf8259
Gustavo <gustavo7dias@gmail.com>
2023-06-15 18:36:56 -0300

    Add new list variant

--- stories/components/List/List.stories.js
+++ stories/components/List/List.stories.js
@@ -1,5 +1,7 @@
 import Primary from "./Primary.html";
 import Secondary from "./Secondary.html";
+import Tertiary from "./Tertiary.html";
+import Quarternary from "./Quarternary.html";
 
 export default {
   title: "Components/List",
@@ -7,3 +9,5 @@ export default {
 
 export const primary = () => Primary;
 export const secondary = () => Secondary;
+export const tertiary = () => Tertiary;
+export const quarternary = () => Quarternary;
