3b91fc7bfb6488f9c6988e32f651d407100c3bee
Gustavo <gustavo7dias@gmail.com>
2023-11-01 20:21:20 -0300

    Create Section layout and update price

---
+++ stories/layouts/Section/Section.stories.js
@@ -0,0 +1,9 @@
+import SectionPrimary from "./SectionPrimary.html";
+import SectionSecondary from "./SectionSecondary.html";
+
+export default {
+  title: "Layouts/Section",
+};
+
+export const primary = () => SectionPrimary;
+export const secondary = () => SectionSecondary;
