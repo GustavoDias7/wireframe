47ed7389e67b21c63498770cbccfb9c40b9d0a17
Gustavo <gustavo7dias@gmail.com>
2024-01-01 13:14:07 -0300

    Update topics adding grid

--- .storybook/preview.js
+++ .storybook/preview.js
@@ -1,8 +1,6 @@
 import "../styles/page/main.css";
 import { themes } from "@storybook/theming";
 
-console.log({ themes });
-
 export const parameters = {
   actions: { argTypesRegex: "^on[A-Z].*" },
   controls: {
