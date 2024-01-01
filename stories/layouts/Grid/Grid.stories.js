6799817c86c1acfa9b084f7d873c3cf13d564948
Gustavo <gustavo7dias@gmail.com>
2024-01-01 13:13:46 -0300

    Update grid system and create examples

--- stories/layouts/Grid/Grid.stories.js
+++ stories/layouts/Grid/Grid.stories.js
@@ -1,8 +1,18 @@
 import GridPrimary from "./Primary.html";
+import GridThreeItems from "./ThreeItems.html";
+import GridNoGap from "./NoGap.html";
+import GridCenter from "./Center.html";
+import GridRight from "./Right.html";
+import GridAuto from "./Auto.html";
 import ejs from "../../../utils/ejs.min.js";
 
 export default {
   title: "Layouts/Grid",
 };
 
-export const Primary = () => ejs.render(GridPrimary, { list: [] });
+export const Primary = () => ejs.render(GridPrimary);
+export const ThreeItems = () => ejs.render(GridThreeItems);
+export const NoGap = () => ejs.render(GridNoGap);
+export const Center = () => ejs.render(GridCenter);
+export const Right = () => ejs.render(GridRight);
+export const Auto = () => ejs.render(GridAuto);
