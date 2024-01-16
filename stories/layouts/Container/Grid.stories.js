1322a628dd9a61c70a374a9308d31fa9694451dd
Gustavo <gustavo7dias@gmail.com>
2024-01-16 13:30:45 -0300

    Update container layout and breakpoints

--- stories/layouts/Container/Grid.stories.js
+++ stories/layouts/Container/Grid.stories.js
@@ -1,8 +1,35 @@
-import Container from "./Primary.html";
+import AllContainers from "./All.html";
+import CustomContainer from "./Custom.html";
 import ejs from "../../../utils/ejs.min.js";
+
+const breakpoints = {
+  xs: 320,
+  sm: 640,
+  md: 768,
+  lg: 1024,
+  xl: 1280,
+};
 
 export default {
   title: "Layouts/Container",
+  argTypes: {
+    breakpoint: {
+      control: "select",
+      options: ["default", ...Object.keys(breakpoints)],
+    },
+  },
+  args: {
+    breakpoint: "default",
+  },
 };
 
-export const container = () => ejs.render(Container);
+export const custom_container = {
+  render: ({ breakpoint }) => {
+    return ejs.render(CustomContainer, {
+      breakpoint,
+      px: breakpoints[breakpoint],
+    });
+  },
+};
+
+export const all_containers = () => ejs.render(AllContainers, { breakpoints });
