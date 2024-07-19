ae3dfa3bc7ad1878d3c2a3a513f2c0323876db22
Gustavo <gustavo7dias@gmail.com>
2024-07-19 16:29:51 -0300

    Create Badge component

---
+++ stories/components/Badge/Badge.stories.js
@@ -0,0 +1,20 @@
+import BadgeHTML from "./Badge.html";
+import ejs from "../../../utils/ejs.min.js";
+
+export default {
+  title: "Components/Badge",
+  args: {
+    active: true,
+  },
+
+  render: (args) => {
+    const classes = ["badge"];
+    if (args.active) classes.push("active");
+    const className = classes.join(" ");
+    return ejs.render(BadgeHTML, { ...args, className });
+  },
+};
+
+export const Badge = {
+  args: {},
+};
