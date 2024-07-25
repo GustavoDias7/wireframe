567519ce832d6b3479c2df732cf4a6bfcbb2fb2d
Gustavo <gustavo7dias@gmail.com>
2024-07-25 20:52:25 -0300

    Create secondary topic

--- stories/components/Topic/Topic.stories.js
+++ stories/components/Topic/Topic.stories.js
@@ -1,8 +1,10 @@
 import TopicPrimary from "./Primary.html";
+import TopicSecondary from "./Secondary.html";
 import ejs from "../../../utils/ejs.min.js";
 
 export default {
   title: "Components/Topic",
 };
 
-export const Primary = () => ejs.render(TopicPrimary, { list: [] });
+export const Primary = () => ejs.render(TopicPrimary);
+export const Secondary = () => ejs.render(TopicSecondary);
