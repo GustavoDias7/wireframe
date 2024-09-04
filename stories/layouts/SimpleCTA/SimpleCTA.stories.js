767a1e7ddbfce6afa2173f8582620d19b01d6e8b
Gustavo <gustavo7dias@gmail.com>
2024-09-04 11:58:42 -0300

    Create SimpleCTA layout

---
+++ stories/layouts/SimpleCTA/SimpleCTA.stories.js
@@ -0,0 +1,19 @@
+import SimpleCTAHTML from "./SimpleCTA.html";
+import ejs from "../../../utils/ejs.min.js";
+
+export default {
+  title: "Layouts/SimpleCTA",
+  args: { color: true },
+  render(args) {
+    const classes = ["simple-cta"];
+
+    if (args.color) classes.push("color");
+
+    const className = classes.join(" ");
+    return ejs.render(SimpleCTAHTML, { ...args, className });
+  },
+};
+
+export const simpleCTA = {
+  args: {},
+};
