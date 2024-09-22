037aaf6c49243b0bbad298d1c36c252cb2901099
Gustavo <gustavo7dias@gmail.com>
2024-09-21 23:28:38 -0300

    Update box-shadow variables

---
+++ stories/components/shadows.stories.js
@@ -0,0 +1,11 @@
+import ShadowsHTML from "../../templates/components/shadows.html";
+
+export default {
+  title: "Components/Shadows",
+};
+
+export const Shadows = {
+  render: () => {
+    return ShadowsHTML;
+  },
+};
