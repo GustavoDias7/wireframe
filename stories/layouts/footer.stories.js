e9e864b81618a63ae134708049ee60775d9fdaca
Gustavo <gustavo7dias@gmail.com>
2024-09-24 13:54:07 -0300

    Create primary and secondary footer layout

--- stories/layouts/footer.stories.js
+++ stories/layouts/footer.stories.js
@@ -1,7 +1,28 @@
-import FooterHTML from "../../templates/layouts/footer.html";
+import PrimaryHTML from "../../templates/layouts/footer/primary.html";
+import SecondaryHTML from "../../templates/layouts/footer/secondary.html";
+import nunjucks from "nunjucks";
+nunjucks.configure({ autoescape: true });
 
 export default {
   title: "Layouts/Footer",
+  args: {
+    whatsapp: true,
+    whatsapp_link: "https://wa.me/552199999999",
+    facebook: true,
+    facebook_link: "https://www.facebook.com/",
+    instagram: true,
+    instagram_link: "https://www.instagram.com/",
+    linkedin: true,
+    linkedin_link: "https://www.linkedin.com/",
+    x: true,
+    x_link: "https://x.com/",
+  },
 };
 
-export const Footer = () => FooterHTML;
+export const PrimaryFooter = () => PrimaryHTML;
+export const SecondaryFooter = {
+  args: {},
+  render(args) {
+    return nunjucks.renderString(SecondaryHTML, { ...args });
+  },
+};
