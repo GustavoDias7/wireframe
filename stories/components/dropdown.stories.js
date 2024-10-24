328593f18be6b8b3bdcd6e27e8bd9ac7bdd018ad
Gustavo <gustavo7dias@gmail.com>
2024-10-24 14:01:30 -0300

    Create dropdown components

---
+++ stories/components/dropdown.stories.js
@@ -0,0 +1,9 @@
+import DesktopHTML from "../../templates/components/dropdown/desktop.html";
+import MobileHTML from "../../templates/components/dropdown/mobile.html";
+
+export default {
+  title: "Components/Dropdown",
+};
+
+export const desktop = () => DesktopHTML;
+export const mobile = () => MobileHTML;
