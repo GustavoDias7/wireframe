90dda8d72dbf00855ec542b3ba57ed54028c70fb
Gustavo <gustavo7dias@gmail.com>
2024-11-20 14:38:08 -0300

    Delete section layout

--- stories/layouts/section.stories.js
+++
@@ -1,9 +0,0 @@
-import PrimaryHTML from "../../templates/layouts/section/primary.html";
-import SecondaryHTML from "../../templates/layouts/section/secondary.html";
-
-export default {
-  title: "Layouts/Section",
-};
-
-export const Primary = () => PrimaryHTML;
-export const Secondary = () => SecondaryHTML;
