7f5234beae1207566bf3ab87429e829099de30a5
Gustavo <gustavo7dias@gmail.com>
2024-11-28 12:56:17 -0300

    Update Header layout

--- stories/layouts/header.stories.js
+++ stories/layouts/header.stories.js
@@ -1,9 +1,45 @@
-import PrimaryHTML from "../../templates/layouts/header/primary.html";
-import SecondaryHTML from "../../templates/layouts/header/secondary.html";
+import HeaderHTML from "../../templates/layouts/header.html";
+import nunjucks from "nunjucks";
+nunjucks.configure({ autoescape: true });
 
 export default {
   title: "Layouts/Header",
-};
+  argTypes: {
+    logo_image: { control: "boolean" },
+    button1: { control: "boolean" },
+    button2: { control: "boolean" },
+    cart: { control: "boolean" },
+    social_links: { control: "boolean" },
+    only_logo: { control: "boolean" },
+    between: { control: "boolean" },
+    color: { control: "boolean" },
+  },
 
-export const Primary = () => PrimaryHTML;
-export const Secondary = () => SecondaryHTML;
+  render(args) {
+    const headerClasses = ["header"];
+    const containerClasses = ["container", "md:container"];
+
+    if (args.between) containerClasses.push("between");
+    if (args.color) headerClasses.push("color");
+
+    const headerClassName = headerClasses.join(" ");
+    const containerClassName = containerClasses.join(" ");
+    return nunjucks.renderString(HeaderHTML, {
+      ...args,
+      containerClassName,
+      headerClassName,
+    });
+  },
+};
+export const Header = {
+  args: {
+    logo_image: true,
+    button1: false,
+    button2: false,
+    cart: false,
+    social_links: false,
+    only_logo: false,
+    between: true,
+    color: false,
+  },
+};
